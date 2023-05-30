83360ea667f9b2f2d6b366cd9e16dade8c9ec5e9
Luca Inzirillo <l_inzirillo@etu-webschoolfactory.fr>
2023-05-30 08:23:07 +0200

    Push node server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,43 @@
-import { greetUser } from '$utils/greet';
+import 'chart.js/auto'; // Assurez-vous d'importer le contrôleur de beignet
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+import { Chart } from 'chart.js';
+
+// eslint-disable-next-line no-console
+console.log('Script loaded'); // Ajouté au début de votre script
+
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line no-console
+  console.log('DOM fully loaded and parsed');
+
+  window.Webflow ||= [];
+  window.Webflow.push(() => {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    const ctx1 = document.querySelector('[data-element="chart-1"]');
+    // eslint-disable-next-line no-console
+    console.log('Canvas element: ', ctx1); // Doit imprimer l'élément canvas ou null s'il n'est pas trouvé
+
+    if (!ctx1 || !(ctx1 instanceof HTMLCanvasElement)) return;
+
+    new Chart(ctx1.getContext('2d'), {
+      type: 'doughnut',
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map((row) => row.count),
+          },
+        ],
+      },
+    });
+  });
 });
