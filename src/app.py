08e26a13102669bfe87782afd569b43ec781feed
Luca Inzirillo <l_inzirillo@etu-webschoolfactory.fr>
2023-06-07 11:40:03 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -11,10 +11,11 @@ def get_data():
 
     # Définir une requête
     query = """
-    SELECT *
+    SELECT Agence_Gerante, Materiel, Date_Mise_en_Service, Statut, Libelle_Statut_Materiel, CA, Nb_Jours_Fac, Valeur_nette_comptable, Prix_Reference_Monnaie_locale
     FROM `b2u-wsf-data-project.loxam.détails_matériel`
     LIMIT 1000
     """
+
 
     # Exécuter la requête et charger les résultats dans un DataFrame
     df = client.query(query).to_dataframe()
